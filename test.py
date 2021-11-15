57d84229301791346bae584a117bb1d606fb8d0c
unknown <bfarr22.jamesbrettfarr@gmail.com>
2021-11-15 11:43:07 -0800

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+groups = [["Jobs", "Gates"], ["Newton", "Euclid"], ["Einstein", "Feynman"]]
+
+for group in groups:
+    for name in group:
+        print(name)
+
+
+for i in range(3):
+  print("WARNING")
+
+  for i in range(3):
+      print(i)
